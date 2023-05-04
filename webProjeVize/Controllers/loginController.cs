01e27ebd5efa8b0bfdcb1b30c5ae6fd2e2ca13ad
mustafagnz <mustafagnz3478@gmail.com>
2023-05-05 00:49:50 +0300

    s

---
+++ webProjeVize/Controllers/loginController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using webProjeVize.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webProjeVize.Controllers
+{
+    public class loginController : Controller
+    {
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+
+        public IActionResult Index(string username, string password)
+        {
+            authentication cslAuth = new authentication();
+
+            if(cslAuth.UsernamePasswordController(username, password))
+            {
+
+                ViewBag.mesaj = "giriş başarılı";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.mesaj = cslAuth.errosMessage;
+            }
+
+            return View();
+        }
+    }
+
+}
+
---
+++ webProjeVize/Models/authentication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace webProjeVize.Models
+{
+	public class authentication
+	{
+        public string errosMessage { get; set; }
+		public bool UsernamePasswordController(string username, string password)
+		{
+            if (username == "mustafa@gmail.com" && password == "1234")
+            {
+                
+                return true;
+
+            }
+            else
+            {
+                errosMessage = "kullanıcı adı bulunamadı";
+                return false;
+            }
+
+        }
+	}
+}
+
